9e7532d9f4538fed26d907ae5faa6da3518a16b7
geekchen <cscbeyond@sina.com>
2019-08-22 23:55:59 +0800

    base-branch

---
+++ lib/demo/basic_demo.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class BasicDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('hello');
+  }
+}
---
+++ lib/model/post.dart
@@ -0,0 +1,63 @@
+class Post {
+  const Post({this.title, this.author, this.imageUrl});
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'ggg',
+    author: 'chen',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'eee',
+    author: 'h',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'eee',
+    author: 'e',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  ),
+  Post(
+    title: 'kkk',
+    author: 'n',
+    imageUrl:
+        'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+  )
+];
